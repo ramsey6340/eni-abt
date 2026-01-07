3f9529191cec6b3bb62f4a77814245e5b7cd2d26
ramsey6340 <98720891+ramsey6340@users.noreply.github.com>
2026-01-07 09:30:24 +0200

    Delete Systeme distribué directory

--- Systeme distribué/TP1-JADE/UniqueAgent/src/UniqueAgent.java
+++
@@ -1,9 +0,0 @@
-import jade.core.Agent;
-
-public class UniqueAgent extends Agent {
-
-    @Override
-    protected void setup() {
-        System.out.println("Je suis un Agent Informatique");
-    }
-}
--- Systeme distribué/TP3-RMI/RMI ADD/Hello.java
+++
@@ -1,5 +0,0 @@
-import java.rmi.*;
-
-public interface Hello extends Remote {
-    int addNumbers(int num1, int num2) throws RemoteException;
-}
